d5da13bf01ae508a9d79a344cfec321b33839266
Chris Pennington <chris@codinginpublic.dev>
2023-12-15 07:39:34 -0700

    finished code

---
+++ src/data/siteData.ts
@@ -0,0 +1,52 @@
+export const siteData = {
+  name: "Gallery Coffee Roaster",
+};
+
+export const socials = [
+  {
+    label: "Instagram",
+    icon: "fe:instagram",
+    href: "https://instagram.com/gallerycoffees",
+  },
+  {
+    label: "Email",
+    icon: "fe:mail",
+    href: "hello@gallerycoffeeroasters.com",
+  },
+  {
+    label: "Facebook",
+    icon: "fe:facebook",
+    href: "https://www.facebook.com/gallerycoffees",
+  },
+];
+
+export const navData = [
+  {
+    name: "shop",
+    href: "shop",
+  },
+  {
+    name: "order ahead",
+    href: "order-ahead",
+  },
+  {
+    name: "wholesale",
+    href: "wholesale",
+  },
+  {
+    name: "home",
+    href: "/",
+  },
+  {
+    name: "about",
+    href: "about",
+  },
+  {
+    name: "contact",
+    href: "contact",
+  },
+  {
+    name: "sign in",
+    href: "sign-in",
+  },
+];
